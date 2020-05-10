9586325112c55bbcbf11d432bfc8241b467cc594
Matheus <matheusdin04@gmail.com>
2020-05-10 19:59:09 -0300

    Creating multiple cards
    Theres a bug that it is allowing duplicate card's front to be saved

--- src/app/controllers/CardController.js
+++ src/app/controllers/CardController.js
@@ -2,140 +2,171 @@ const Card = require('../models/Card');
 const UserCollectionDeck = require('../models/UserCollectionDeck');
 const CardFace = require('../models/CardFace');
 const DeckCard = require('../models/DeckCard');
-const File = require('../models/File');
 const CardFaceContents = require('../models/CardFaceContents');
 const Yup = require('yup');
 const ResponseHandlers = require('../../utils/ResponseHandlers');
 class CardController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      deck: Yup.number('The deck must be a number').required(
-        'The deck field is required'
-      ),
-      collection: Yup.number('The collection must be a number').required(
-        'The collection field is required'
-      ),
-      starred: Yup.boolean('The starred must be a boolean'),
-      front: Yup.object({
-        text_content: Yup.string('The text content must be a string').required(
-          'The text content is required'
+      cards: Yup.array()
+        .min(1, 'You must save at least one card')
+        .of(
+          Yup.object({
+            deck: Yup.number('The deck must be a number').required(
+              'The deck field is required'
+            ),
+            collection: Yup.number('The collection must be a number').required(
+              'The collection field is required'
+            ),
+            starred: Yup.boolean('The starred must be a boolean'),
+            front: Yup.object({
+              text_content: Yup.string(
+                'The text content must be a string'
+              ).required('The text content is required'),
+              medias: Yup.array().of(Yup.number('Medias must be a number')),
+            }),
+            back: Yup.object({
+              text_content: Yup.string(
+                'The text content must be a string'
+              ).required('The text content is required'),
+              medias: Yup.array().of(Yup.number('Medias must be a number')),
+            }),
+          })
         ),
-        medias: Yup.array().of(Yup.number('Medias must be a number')),
-      }),
-      back: Yup.object({
-        text_content: Yup.string('The text content must be a string').required(
-          'The text content is required'
-        ),
-        medias: Yup.array().of(Yup.number('Medias must be a number')),
-      }),
     });
 
     schema
       .validate(req.body, { abortEarly: false })
       .then(async (_) => {
         try {
-          const { deck, collection, starred, front, back } = req.body;
+          const { cards } = req.body;
 
-          const userHasDeckAndCollection = await UserCollectionDeck.findOne({
-            where: {
-              user: req.userId,
-              deck: deck,
-              collection: collection,
-            },
-          });
+          const saveCardsPromises = cards.map(async (cardData) => {
+            const { deck, collection, starred, front, back } = cardData;
 
-          if (!userHasDeckAndCollection) {
-            return res.status(401).json({
-              code: 401,
-              error: {
-                field: 'collection/deck',
-                message:
-                  'The provided collection/deck does not belong to the user',
+            const userHasDeckAndCollection = await UserCollectionDeck.findOne({
+              where: {
+                user: req.userId,
+                deck: deck,
+                collection: collection,
               },
-              message:
-                'You are not allowed to insert cards on the provided collection and deck',
             });
-          }
 
-          const frontContentAlreadyExists = await Card.findOne({
-            include: [
-              {
-                model: CardFace,
-                as: 'front_face',
-                where: {
-                  text_content: front.text_content,
+            if (!userHasDeckAndCollection) {
+              return res.status(401).json({
+                code: 401,
+                error: {
+                  field: 'collection/deck',
+                  message:
+                    'The provided collection/deck does not belong to the user',
                 },
-              },
-            ],
-          });
-
-          if (frontContentAlreadyExists) {
-            return res.status(400).json({
-              code: 400,
-              error: {
-                field: 'front',
-                message: 'The front of this card is a duplicate',
-              },
-              message: 'Front card duplicate found',
-            });
-          }
-
-          const [frontData, backData] = await Promise.all([
-            CardFace.create({ text_content: front.text_content }),
-            CardFace.create({ text_content: back.text_content }),
-          ]);
-
-          var frontMedias = null;
-          var backMedias = null;
-          if(typeof front.medias !== 'undefined') {
-            frontMedias = front.medias.map(function (mId) {
-              return {
-                card_face: frontData.id,
-                file: mId,
-              };
-            });
-          }
-
-          if(typeof back.medias !== 'undefined') {
-            backMedias = back.medias.map(function (mId) {
-              return {
-                card_face: backData.id,
-                file: mId,
-              };
-            });
-          }
-
-          if(frontMedias || backMedias) {
-            var medias = [];
-            if(frontMedias) medias = medias.concat(frontMedias)
-            if(backMedias) medias = medias.concat(backMedias);
-
-            if(medias.length >= 0) {
-              await CardFaceContents.bulkCreate(medias);
+                message:
+                  'You are not allowed to insert cards on the provided collection and deck',
+              });
             }
-          }
+            console.log(front.text_content)
+            const frontContentAlreadyExists = await DeckCard.findOne({
+              where: {
+                deck: deck,
+                creator: req.userId,
+              },
+              include: [
+                {
+                  model: Card,
+                  include: [
+                    {
+                      model: CardFace,
+                      as: 'front_face',
+                      where: {
+                        text_content: front.text_content,
+                      },
+                    },
+                  ],
+                },
+              ],
+            });
 
-          const card = await Card.create({
-            starred,
-            front: frontData.id,
-            back: backData.id,
-          });
+            if (frontContentAlreadyExists.dataValues.Card) {
+              return res.status(400).json({
+                code: 400,
+                error: {
+                  field: 'front',
+                  message: 'The front of this card is a duplicate',
+                },
+                message: 'Front card duplicate found',
+              });
+            }
 
-          await DeckCard.create({
-            creator: req.userId,
-            deck,
-            card: card.id,
-          });
+            const [frontData, backData] = await Promise.all([
+              CardFace.create({ text_content: front.text_content }),
+              CardFace.create({ text_content: back.text_content }),
+            ]);
 
-          return res.status(201).json({
-            code: 201,
-            data: {
+            var frontMedias = null;
+            var backMedias = null;
+            if (typeof front.medias !== 'undefined') {
+              frontMedias = front.medias.map(function (mId) {
+                return {
+                  card_face: frontData.id,
+                  file: mId,
+                };
+              });
+            }
+
+            if (typeof back.medias !== 'undefined') {
+              backMedias = back.medias.map(function (mId) {
+                return {
+                  card_face: backData.id,
+                  file: mId,
+                };
+              });
+            }
+
+            if (frontMedias || backMedias) {
+              var medias = [];
+              if (frontMedias) medias = medias.concat(frontMedias);
+              if (backMedias) medias = medias.concat(backMedias);
+
+              if (medias.length >= 0) {
+                await CardFaceContents.bulkCreate(medias);
+              }
+            }
+
+            const card = await Card.create({
+              starred,
+              front: frontData.id,
+              back: backData.id,
+            });
+
+            await DeckCard.create({
+              creator: req.userId,
+              deck,
+              card: card.id,
+            });
+
+            return {
               card,
               collection,
               deck,
-            },
-            message: 'The card was saved successfully',
+            };
           });
+
+          Promise.all(saveCardsPromises)
+            .then((result) => {
+              return res.status(201).json({
+                code: 201,
+                data: result,
+                message: 'All of the cards were saved successfuly',
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+              return res.status(500).json({
+                code: 500,
+                error: err,
+                message: 'An error ocurred while saving the cards',
+              });
+            });
         } catch (err) {
           console.log(err);
           return res.status(500).json({
@@ -152,6 +183,17 @@ class CardController {
           message: 'The fields you provided are not valid',
         });
       });
+  }
+
+  async userHasCollectionAndDeck(req, res, deck, collection) {}
+
+  async frontContentAlreadyExistsFunction(req, res, deck, front) {}
+
+  async saveCardMedias(front, back) {
+    return {
+      frontData,
+      backData,
+    };
   }
 }
 
