07dabe7c219c3ea80f2b58e1e9620763add35efb
Matheus <matheusdin04@gmail.com>
2020-08-08 16:31:51 -0300

    Creating the static collection decks page

---
+++ frontend/soudain/lib/core/commom_widgets/header_with_title_and_subtitle.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:soudain/core/responsiveness/device_size_adapter.dart';
+import 'package:soudain/injection_container.dart';
+
+class HeaderWithTitleAndSubtitle extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Function backFunction;
+
+  HeaderWithTitleAndSubtitle({this.title,this.subtitle,this.backFunction});
+
+  @override
+  Widget build(BuildContext context) {
+    double backArrowSize = sl<DeviceSizeAdapter>().getResponsiveSize(
+      context: context,
+      portraitSizeAdapter: SizeAdapter(
+        isHeight: false,
+        smallPorcentage: 8,
+        mediumPorcentage: 7,
+        largePorcentage: 6
+      ),
+    );
+    return Container(
+      margin: EdgeInsets.only(
+        top: 16,
+        bottom: 6,
+        right: 10,
+        left: 10
+      ),
+      child: Row(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () => this.backFunction(),
+            child: Container(
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
+                size: backArrowSize,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  this.title,
+                  style: GoogleFonts.comfortaa(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 22
+                  ),
+                ),
+                Text(
+                  this.subtitle,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.comfortaa(
+                    color: Colors.white,
+                    fontSize: 16
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- frontend/soudain/lib/features/collection_decks/presentation/pages/collection_decks_page.dart
+++ frontend/soudain/lib/features/collection_decks/presentation/pages/collection_decks_page.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:soudain/core/commom_widgets/deck_format.dart';
+import 'package:soudain/core/commom_widgets/header_with_title_and_subtitle.dart';
+import 'package:soudain/core/commom_widgets/study_deck_front.dart';
+import 'package:soudain/core/commom_widgets/title_content_section.dart';
 import 'package:soudain/core/constants/colors.dart';
+import 'package:soudain/core/responsiveness/device_size_adapter.dart';
+import 'package:soudain/features/home/presentation/widgets/oval_red_ball.dart';
+import 'package:soudain/features/navigation/bloc/navigation_bloc.dart';
 import 'package:soudain/features/play/data/models/collection_data.dart';
+import 'package:soudain/injection_container.dart';
 
 class CollectionDecksPage extends StatelessWidget {
   final CollectionData collection;
@@ -9,22 +19,178 @@ class CollectionDecksPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double addButtomMargin = sl<DeviceSizeAdapter>().getResponsiveSize(
+      context: context,
+      portraitSizeAdapter: SizeAdapter(
+        isHeight: false,
+        smallPorcentage: 30,
+        mediumPorcentage: 30,
+        largePorcentage: 10
+      )
+    );
+    double cardsSectionHeight = sl<DeviceSizeAdapter>().getResponsiveSize(
+      context: context,
+      portraitSizeAdapter: SizeAdapter(
+        isHeight: true,
+        smallPorcentage: 40,
+        mediumPorcentage: 35,
+        largePorcentage: 30
+      )
+    );
     return SafeArea(
       child: Scaffold(
         backgroundColor: secondaryColor,
-        body: Center(
-          child: Container(
-            height: 100,
-            width: 100,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              HeaderWithTitleAndSubtitle(
+                title: collection.title,
+                subtitle: 'Collection',
+                backFunction: () => BlocProvider.of<NavigationBloc>(context).add(PopEvent()),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    left: 10,
+                    right: 10,
+                    top: 4
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.3,
+                          child: CollectionDetailsWidget(),
+                        ),
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.14,
+                          margin: EdgeInsets.only(
+                            left: addButtomMargin,
+                            right: addButtomMargin,
+                            top: 10
+                          ),
+                          child: DeckFormat(
+                            centerWidget: OvalRedBall(
+                              icon: Icons.add,
+                            ),
+                            cardsMargin: 10,
+                            cardBorderRadius: 20,
+                            cardColor: Colors.white,
+                            secondaryColor: Colors.white,
+                            onPressed: () => null,
+                            isLeftMargin: true,
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Container(
+                          padding: EdgeInsets.only(left: 10),
+                          height: cardsSectionHeight,
+                          child: TitleContentSection(
+                            title: 'Decks to review',
+                            leftPadding: 0,
+                            content: ListView.separated(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 4,
+                              separatorBuilder: (_,i) => SizedBox(width: 20,),
+                              itemBuilder: (_,index) => Container(
+                                width: MediaQuery.of(context).size.width * 0.45,
+                                margin: EdgeInsets.only(
+                                    right: index == 3 ? 10 : 0
+                                ),
+                                child: DeckFormat(
+                                  centerWidget: StudyDeckFront(),
+                                  cardsMargin: 10,
+                                  cardBorderRadius: 20,
+                                  cardColor: Colors.white,
+                                  secondaryColor: Colors.white,
+                                  onPressed: null,
+                                  isLeftMargin: true,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Container(
+                          padding: EdgeInsets.only(left: 10),
+                          margin: EdgeInsets.only(bottom: 20),
+                          height: cardsSectionHeight,
+                          child: TitleContentSection(
+                            title: 'Decks to review',
+                            leftPadding: 0,
+                            content: ListView.separated(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 4,
+                              separatorBuilder: (_,i) => SizedBox(width: 20,),
+                              itemBuilder: (_,index) => Container(
+                                width: MediaQuery.of(context).size.width * 0.45,
+                                margin: EdgeInsets.only(
+                                    right: index == 3 ? 10 : 0
+                                ),
+                                child: DeckFormat(
+                                  centerWidget: StudyDeckFront(),
+                                  cardsMargin: 10,
+                                  cardBorderRadius: 20,
+                                  cardColor: Colors.white,
+                                  secondaryColor: Colors.white,
+                                  onPressed: null,
+                                  isLeftMargin: true,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget CollectionDetailsWidget() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 6,
+          child: Card(
+            margin: EdgeInsets.only(
+              right: 10,
+              top: 20,
+              bottom: 20
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20))
+            ),
+            elevation: 4,
             child: Hero(
               tag: collection.id,
               child: Image.network(
-                collection.imageUrl
+                collection.imageUrl,
+                fit: BoxFit.cover,
               ),
             ),
           ),
         ),
-      ),
+        Expanded(
+          flex: 8,
+          child: Text(
+            collection.description,
+            textAlign: TextAlign.justify,
+            maxLines: 8,
+            overflow: TextOverflow.ellipsis,
+            style: GoogleFonts.comfortaa(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+            ),
+          ),
+        )
+      ],
     );
   }
 
